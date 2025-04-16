349ab4b03fb09a483a5a8496039850739e83383a
bp_yskim3 <yskim@digicaps.com>
2025-04-16 16:45:30 +0900

    application profile별 설정 분리 및 react cors 추가

--- src/main/java/org/arc/auth/config/SecurityConfig.java
+++ src/main/java/org/arc/auth/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.arc.auth.security.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,6 +18,7 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http, JwtFilter jwtFilter) throws Exception {
     http.csrf(csrf -> csrf.disable())
+        .cors(Customizer.withDefaults())
         .authorizeHttpRequests(auth -> auth
             .requestMatchers(
                 "/swagger-ui/**",
---
+++ src/main/java/org/arc/auth/config/WebConfig.java
@@ -0,0 +1,28 @@
+package org.arc.auth.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+  @Value("${front.url}")
+  String url;
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 엔드포인트 허용
+            .allowedOrigins(url) // 프론트 주소
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+      }
+    };
+  }
+}
