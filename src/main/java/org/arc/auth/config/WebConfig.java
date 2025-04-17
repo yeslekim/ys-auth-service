434c826d74caab76afb399ed8a2315693fb7ec5a
bp_yskim3 <yskim@digicaps.com>
2025-04-17 11:43:29 +0900

    spring security cors 설정 추가

--- src/main/java/org/arc/auth/config/WebConfig.java
+++ src/main/java/org/arc/auth/config/WebConfig.java
@@ -1,8 +1,12 @@
 package org.arc.auth.config;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,11 +22,24 @@ public class WebConfig {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 엔드포인트 허용
-            .allowedOrigins("*") // 프론트 주소
+            .allowedOrigins(url) // 프론트 주소
             .allowedMethods("*")
             .allowedHeaders("*")
             .allowCredentials(true);
       }
     };
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(List.of(url));
+    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    config.setAllowedHeaders(List.of("*"));
+    config.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+    return source;
   }
 }
